99c08cf552452a87c488d3385dbef78b38b9dfb7
night-king <yemor@qq.com>
2021-12-04 11:36:58 +0800

    init

---
+++ scripts/prepare_upgrade.js
@@ -0,0 +1,17 @@
+// scripts/prepare_upgrade.js
+async function main() {
+    const proxyAddress = '0x3F1e8477418Ca934ACE64E4524eD58b3aE374F2b';
+   
+    const BoxV2 = await ethers.getContractFactory("BoxV2");
+    console.log("Preparing upgrade...");
+    const boxV2Address = await upgrades.prepareUpgrade(proxyAddress, BoxV2);
+    console.log("BoxV2 at:", boxV2Address);
+  }
+   
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+  
---
+++ scripts/transfer_ownership.js
@@ -0,0 +1,17 @@
+// scripts/transfer_ownership.js
+async function main() {
+    const gnosisSafe = '0x8858Dbd50a3364ff97d7387FcbB8Be694AA56717';
+   
+    console.log("Transferring ownership of ProxyAdmin...");
+    // The owner of the ProxyAdmin can upgrade our contracts
+    await upgrades.admin.transferProxyAdminOwnership(gnosisSafe);
+    console.log("Transferred ownership of ProxyAdmin to:", gnosisSafe);
+  }
+   
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+  
